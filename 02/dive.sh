d437bb5640d4483eba1f5e3538e9573a61d910d2
lytixdev <nicolaihbrand@gmail.com>
2021-12-02 10:09:58 +0100

    finished day 2👻👻

---
+++ 02/dive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+horisontal=0
+vertical=0
+aim=0
+
+while read -r line;
+do
+    cmd=$(echo $line | awk '{print $1}')
+    val=$(echo $line | awk '{print $2}')
+    [ "$cmd" == "forward" ] && horisontal=$((horisontal+val))
+    [ "$cmd" == "down" ] && vertical=$((vertical+val))
+    [ "$cmd" == "up" ] && vertical=$((vertical-val))
+done < "input.txt"
+
+echo $((horisontal*vertical))
