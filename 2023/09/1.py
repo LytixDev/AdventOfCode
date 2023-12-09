7eeced5bec0c53991dd3c09aa21cf429b11dba6b
Nicolai Brand <nicolahb@stud.ntnu.no>
2023-12-09 07:43:51 +0100

    day 9

---
+++ 2023/09/1.py
@@ -0,0 +1,24 @@
+with open("input") as f:
+    d = [l.strip() for l in f.readlines()]
+
+# brute force
+s = 0
+for l in d:
+    values = [int(v) for v in l.split(" ")]
+    a = [values]
+    while True:
+        if a[-1].count(0) == len(a[-1]):
+            a.pop()
+            break
+        A = []
+        for idx in range(len(a[-1]) - 1):
+            i, j = a[-1][idx:idx+2]
+            A.append(j - i)
+        a.append(A)
+
+    pred = 0
+    for A in a[::-1]:
+        pred += A[-1]
+    s += pred
+
+print(s)
